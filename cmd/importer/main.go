702a42e2b2638bd63bfc65df0b7ae7cde9993290
Juan Francisco Giménez Silva <juanfgs@gmail.com>
2025-06-15 23:09:32 -0300

    Modified importer to use loader and import entities in DB

--- cmd/importer/main.go
+++ cmd/importer/main.go
@@ -1,12 +1,41 @@
-package main 
+package main
 
 import (
 	"github.com/juanfgs/dnd-monster-library/internal/config"
 	"github.com/juanfgs/dnd-monster-library/internal/db"
+	"github.com/juanfgs/dnd-monster-library/internal/monster"
+	"github.com/juanfgs/dnd-monster-library/internal/stats"
+	"github.com/juanfgs/dnd-monster-library/internal/loader"
+	"context"
+	"log"
 )
+
 
 func main() {
 	var config = config.NewConfig()
+	monsters := loader.LoadMonsters()
 	config.ReadEnv()
-	db.Connect(config.DSN)
+	pool, err := db.Connect(config.DSN)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	monsterRepo := monster.NewRepository(pool)
+
+	statsRepo := stats.NewRepository(pool)
+	ctx := context.Background()
+	for _, m := range(monsters) {
+		log.Println(m)
+		err := monsterRepo.Create(ctx, &m)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = statsRepo.Create(ctx, m.Stats, m.ID)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	
 }
+
